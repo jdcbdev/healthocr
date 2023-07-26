393832ed2fc77e77fee4fa0b96f051614ae30154
mareekkk <11805713+mareekkk@users.noreply.github.com>
2023-07-26 18:03:33 +0000

    added home, mobile and work phones, added address, add age calculation based on birthdate

--- ocr/process.py
+++ ocr/process.py
@@ -4,34 +4,63 @@ from celery import shared_task
 from healthocr.celery import app
 import math
 from .models import MedicalRecord
+from datetime import datetime
+import datetime
 
 # Set OpenAI key
 openai.api_key = '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'
 
 def generate_prompt(text):
-    prompt = f"The text is a medical document:\n\n{text}\n\nIdentify name, birthdate, and age from this document."
+    prompt = f"The text is a medical document:\n\n{text}\n\n"\
+    "Identify the following information from this document:\n"\
+    "1. Name\n"\
+    "2. D.O.B.\n"\
+    "3. Record No.\n"\
+    "4. Address\n"\
+    "5. Home Phone\n"\
+    "6. Mobile Phone\n"\
+    "7. Work Phone"
     return prompt
 
 def parse_gpt3_output(gpt3_output):
     lines = gpt3_output.split('\n')
 
     name = None
-    birthdate = None
-    age = None
+    dob = None
+    record_no = None
+    address = None
+    home_phone = ''
+    mobile_phone = ''
+    work_phone = ''
 
     for line in lines:
         if "Name:" in line:
             name = line.replace("Name:", "").strip()
-        elif "Birthdate:" in line:
-            birthdate_str = line.replace("Birthdate:", "").strip()
-            birthdate_str = birthdate_str.replace("(DD/MM/YYYY)", "").strip()  # Remove date format hint
-            birthdate = parse_date(birthdate_str)
-        elif "Age:" in line:
-            age_str = line.replace("Age:", "").strip()
-            age = ''.join(filter(str.isdigit, age_str))
-            age = int(age) if age else None
+        elif "D.O.B.:" in line:
+            dob_str = line.replace("D.O.B.:", "").strip()
+            dob_str = dob_str.replace("(DD/MM/YYYY)", "").strip()  # Remove date format hint
+            try:
+                dob = parse_date(dob_str)
+            except Exception as e:
+                print(f"Could not parse date: {e}")
+                dob = None
+        elif "Record No.:" in line:
+            record_no = line.replace("Record No.:", "").strip()
+        elif "Address:" in line:
+            address = line.replace("Address:", "").strip()
+        elif "Home Phone:" in line:
+            home_phone = line.replace("Home Phone:", "").strip()
+        elif "Mobile Phone:" in line:
+            mobile_phone = line.replace("Mobile Phone:", "").strip()
+        elif "Work Phone:" in line:
+            work_phone = line.replace("Work Phone:", "").strip()
 
-    return name, birthdate, age
+    # Now we calculate the age based on dob
+    if dob:
+        today = datetime.date.today()
+        age = today.year - dob.year - ((today.month, today.day) < (dob.month, dob.day))
+
+    return name, dob, age, address, home_phone, mobile_phone, work_phone
 
 @app.task
 def extract_info_with_gpt3(text, record_id):
@@ -49,33 +78,50 @@ def extract_info_with_gpt3(text, record_id):
     all_names = []
     all_birthdates = []
     all_ages = []
+    all_addresses = []
+    all_home_phones = []
+    all_mobile_phones = []
+    all_work_phones = []
+
     for text_chunk in text_chunks:
         # Generate a suitable prompt for GPT-3 based on the input text
         messages = [
             {"role": "system", "content": "The text is a medical document."},
             {"role": "user", "content": text_chunk},
-            {"role": "assistant", "content": "Identify name, birthdate, and age from this document."},
+            {"role": "assistant", "content": "Identify name, birthdate, age, address, mobile phone, home phone and work phone from this document."},
         ]
 
         # Use the GPT-3 "davinci" model to generate a response
         response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages, max_tokens=2000)
 
         # Extract info from GPT-3 response
-        name, birthdate, age = parse_gpt3_output(response['choices'][0]['message']['content'].strip())
+        name, birthdate, age, address, home_phone, mobile_phone, work_phone = parse_gpt3_output(response['choices'][0]['message']['content'].strip())
         all_names.append(name)
         all_birthdates.append(birthdate)
         all_ages.append(age)
+        all_addresses.append(address)
+        all_home_phones.append(home_phone)
+        all_mobile_phones.append(mobile_phone)
+        all_work_phones.append(work_phone)
 
     # Combine extracted info from all chunks
     name = max(set(all_names), key=all_names.count)
     birthdate = max(set(all_birthdates), key=all_birthdates.count)
     age = max(set(all_ages), key=all_ages.count)
+    address = max(set(all_addresses), key=all_addresses.count)
+    home_phone = max(set(all_home_phones), key=all_home_phones.count)
+    mobile_phone = max(set(all_mobile_phones), key=all_mobile_phones.count)
+    work_phone = max(set(all_work_phones), key=all_work_phones.count)
 
     # Update the MedicalRecord with the extracted data
     record = MedicalRecord.objects.get(id=record_id)
     record.name = name
     record.birthdate = birthdate
     record.age = age
+    record.address = address
+    record.home_phone = home_phone
+    record.work_phone = work_phone
+    record.mobile_phone = mobile_phone
     record.save()
 
-    return name, birthdate, age
+    return name, birthdate, age, address, home_phone, mobile_phone, work_phone
